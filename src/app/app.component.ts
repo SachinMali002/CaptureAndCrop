d82ba1f7cce66b8711c2b93c6f2579440a67730b
Sachin Mali <sachinmali002@gmail.com>
2023-03-13 20:49:30 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ImageCroppedEvent } from 'ngx-image-cropper';
+import { WebcamImage } from 'ngx-webcam';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CaptureAndCrop';
+  imgChangeEvt: any = '';
+  cropImgPreview: any = '';
+  IsCapture = true;
+  BtnCapture = "Capture";
+  // onFileChange(event: any): void {
+  //   this.imgChangeEvt = event;
+  // }
+  cropImg(e: ImageCroppedEvent) {
+    this.cropImgPreview = e.base64;
+  }
+
+
+  private trigger: Subject<any> = new Subject();
+  public camImage!: WebcamImage;
+  private nextWebcam: Subject<any> = new Subject();
+  capturedImage = '';
+  ngOnInit() {
+    console.log("cropImgPreview" + this.cropImgPreview);
+  }
+  public captureImage(): void {
+    this.trigger.next(void 0);
+  }
+  public captureImg(camImage: WebcamImage): void {
+    // this.webcamImage = webcamImage;
+    this.capturedImage = camImage!.imageAsDataUrl;
+    this.imgChangeEvt = this.capturedImage;
+    console.log("IsCapture : " + this.IsCapture);
+    if (camImage!.imageAsDataUrl != null || camImage!.imageAsDataUrl != undefined)
+      this.IsCapture = false;
+    console.log("IsCapture after : " + this.IsCapture);
+    // this.trigger.complete();
+  }
+  public get invokeObservable(): Observable<any> {
+    return this.trigger.asObservable();
+  }
+  public get nextWebcamObservable(): Observable<any> {
+    return this.nextWebcam.asObservable();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ImageCropperModule} from 'ngx-image-cropper'
 import { AppComponent } from './app.component';
+import { WebcamModule } from 'ngx-webcam'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ImageCropperModule,
+    WebcamModule
   ],
   providers: [],
   bootstrap: [AppComponent]
